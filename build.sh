26c0265b7a6818cff134fe9b669a8b1f2df97304
杜膳辰 <dsc@macdeMacBook-Pro-5.local>
2019-08-01 17:37:05 +0800

    gogo

---
+++ build.sh
@@ -0,0 +1,14 @@
+service supervisor start
+service nginx start
+
+cd /root/project/YongDan
+git fetch 
+git merge origin/master
+
+cd project
+python3 manage.py migrate --settings=project.settings_prod
+python3 manage.py collectstatic --settings=project.settings_prod
+
+
+supervisorctl restart project
+service nginx restart
